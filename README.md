e4666dfd60c879a9c0510f6f03d6d7605502ffe8
Simon Slamka <51794014+simonSlamka@users.noreply.github.com>
2022-04-06 16:30:27 +0200

    feat: :sparkles: Write core code
    Written the core of the program

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Cyclify
- A simple menstrual cycle tracker made for @Ongakken's latest game, RackJacker. Even though there are many of these nowadays, we wanted to write our own
+## Cyclify
+A very simple menstrual cycle calculator intended for scripting purposes and not direct user access.
+At the moment, it only calculates a very rough, impersonalized estimate of the next cycle.
+Later versions are planned to include more personalized estimates based on several previous occurences.
---
+++ cyclify.py
@@ -0,0 +1,23 @@
+'''
+CYCLIFY
+A very simple program that calculates the approximate occurance of a menstrual cycle based on the last one.
+Will be updated to use more than one of previous occurences and thus give a better estimate.
+The current use of this is mainly for scripting purposes where this program would act like a module to quickly calculate some values.
+There's no expected direct usage from regular users.
+'''
+
+from datetime import datetime, timedelta
+
+bLoop = 1
+while(bLoop == 1):
+    try:
+        date_last_cycle = datetime.strptime(
+            input("\nPlease enter the date of your last period [mm/dd/yyyy]:\n"), "%m/%d/%Y")
+        print("\n----------\nTo verify, your last period was on:",
+              date_last_cycle.strftime("%A, %m/%d/%Y"), " correct (Y or N)?\n")
+        if(input("") == "Y"):
+            print("Your next period is expected to begin within 4 days of " + str(datetime.strftime(
+                date_last_cycle + timedelta(days=28), "%A, %m/%d/%Y")))  # day not displaying correctly
+            bLoop = 0
+    except Exception as err:
+        print(f"{err}")
